5f79c2068882e6b5250f2f24507195c9b90fd46e
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-02-17 12:20:07 -0800

    started my project just need to get the styling done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Nasa from './Nasa'
+//step 1
 import "./App.css";
-
 
 
 
 function App() {
   const [nasaPic, setNasaPic] = useState();
+
   useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
     .then(response => {
@@ -27,14 +28,7 @@ function App() {
       {nasaPic && <Nasa nasaPic ={nasaPic} />}
     </div>
   ); 
-  // return (
-  //   <div className="App">
-  //     <p>
-  //       Read through the instructions in the README.md file to build your NASA
-  //       app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-  //     </p>
-  //   </div>
-  // );
+  
 
 }
 
--- src/Nasa.js
+++ src/Nasa.js
@@ -1,18 +1,43 @@
 import React from "react";
 
+import styled from 'styled-components'
+
+const StyledNasa = styled.div `
+width: 50% 
+
+
+`
+
+const Headline = styled.h2`
+  color: black;
+  border-bottom:5px solid red;
+`
+const Headline1 = styled.h1`
+    
+  align-items: center; 
+  border-bottom:2px solid gray;
+  width: 100%; 
+  color: blue;
+  font-size: 50px; 
+`;
+
+
+
+
 //i want to created a something here that grabs the data from the Nasa API 
 
 const Nasa = props => {
     return (
-        <div className = "nasa-wrapper">
-            <h1>NASA Picture of the Day!</h1>
-            <h2> {props.nasaPic.title} </h2>
-        <iframe width='800' height='400' src= {props.nasaPic.url} type = {props.nasaPic.media_type} />
+        <StyledNasa className = "nasa-wrapper">
+            
+            <Headline1>NASA Picture of the Day!</Headline1>
+            <Headline> {props.nasaPic.title} </Headline>
+        <img src= {props.nasaPic.url} alt='pic' />
         
         <p> {props.nasaPic.date} </p>
         <p> {props.nasaPic.explanation} </p>
         <p> {props.nasaPic.copyright} </p>
-        </div>
+        </StyledNasa>
         
     )
 
